f8bfec736182c3c17a7d40fe199b02bdee5ef71c
JayDeeTay <skymapdevs@gmail.com>
2026-03-11 12:00:43 -0400

    Trying to get config right.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // [https://vite.dev/config/](https://vite.dev/config/)
 export default defineConfig({
+  base: '/', // Set to '/' for root domain deployments like sky-map-team.github.io
   plugins: [
     react(),
     tailwindcss(),
